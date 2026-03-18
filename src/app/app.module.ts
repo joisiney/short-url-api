435fe25340100086a097c6298b44a292b4392eab
Joisiney Leandro <joisiney@gmail.com>
2026-03-18 14:23:29 -0300

    feat: Implement PostgreSQL database integration using Drizzle ORM with initial schema and migration setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '../config/config.module';
 import { ShortUrlModule } from '../modules/short-url/short-url.module';
+import { DatabaseModule } from '../infra/database/database.module';
 
 @Module({
-  imports: [ConfigModule, ShortUrlModule],
+  imports: [ConfigModule, DatabaseModule, ShortUrlModule],
 })
 export class AppModule {}
---
+++ src/infra/database/migrations/0000_stale_pandemic.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "short_urls" (
+	"id" uuid DEFAULT gen_random_uuid() NOT NULL,
+	"url" text NOT NULL,
+	"short_code" varchar(32) NOT NULL,
+	"access_count" integer DEFAULT 0 NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "pk_short_urls" PRIMARY KEY("id"),
+	CONSTRAINT "uq_short_urls_short_code" UNIQUE("short_code"),
+	CONSTRAINT "ck_short_urls_access_count_non_negative" CHECK ("short_urls"."access_count" >= 0)
+);
